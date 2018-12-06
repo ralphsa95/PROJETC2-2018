41d330506e2a88c2433f33322a8537b20e649438
JoelleTannous <joelle.tannous@isae.edu.lb>
2018-12-06 19:26:53 +0200

    add: history - logs - course & some modifications

--- src/java/operation/SessionBean.java
+++ src/java/operation/SessionBean.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
@@ -84,7 +85,7 @@ public class SessionBean {
     public ArrayList<Degree> getDegree(String filter, String department) {
         ArrayList<Degree> list = new ArrayList();
         try {
-            String sql = "SELECT d.code, d.name name, de.code department ,de.name departmentName, d.credits, d.price,"
+            String sql = "SELECT d.code, d.name name, de.code department ,de.name departmentName, d.price,"
                     + "d.activated  "
                     + " FROM degree d, department de "
                     + " where de.code = d.department"
@@ -102,7 +103,7 @@ public class SessionBean {
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 Degree d = new Degree(rs.getString(1), rs.getString(2), rs.getString(3),
-                        rs.getString(4), rs.getInt(5), rs.getDouble(6), rs.getBoolean(7));
+                        rs.getString(4), rs.getDouble(5), rs.getBoolean(6));
                 list.add(d);
             }
 
@@ -112,13 +113,33 @@ public class SessionBean {
         return list;
     }
 
+    public void manageDegree(Degree d, String action) throws SQLException {
+        String sql = "";
+        if ("U".equals(action)) {
+            sql = "UPDATE degree SET name=?,price=?,department=?,updated_date=now(),updated_by=? WHERE code = ?";
+        } else if ("C".equals(action)) {
+            sql = "Insert into degree (name, price, department, created_date, created_by, activated, code) "
+                    + " values(?,?,?,now(),?,1,?)";
+        }
+
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setString(1, d.getName());
+        stmt.setDouble(2, d.getPrice());
+        stmt.setString(3, d.getDepartment());
+        stmt.setString(4, SessionUtil.getUserCode());
+        stmt.setString(5, d.getCode());
+
+        stmt.executeUpdate();
+
+    }
+
     /**
      * ****************************DegreesCourse********************************
      */
     public ArrayList<DegreeCourse> getDegreeCourse(String degree) {
         ArrayList<DegreeCourse> list = new ArrayList();
         try {
-            String sql = "Select DC.degree, DC.course, DC.semester, C.credits, D.name, C.name"
+            String sql = "Select dc.degree, dc.course, dc.semester, c.credits, d.name, c.name"
                     + "     From degree d, course c, degreecourse dc"
                     + "    where dc.degree = d.code"
                     + "      and dc.course = c.code "
@@ -141,7 +162,7 @@ public class SessionBean {
     public ArrayList<Course> getCoursesForDegee(String degree) {
         ArrayList<Course> list = new ArrayList();
         try {
-            String sql = "Select C.code, C.name"
+            String sql = "Select c.code, c.name"
                     + "     From course c"
                     + "    where activated"
                     + "      and c.code not in(select course from degreecourse where degree = ?)";
@@ -149,7 +170,7 @@ public class SessionBean {
             ps.setString(1, degree);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                Course c = new Course(rs.getString(1), rs.getString(2), 0, null);
+                Course c = new Course(rs.getString(1), rs.getString(2), 0, null, null);
                 list.add(c);
             }
 
@@ -159,28 +180,25 @@ public class SessionBean {
         return list;
     }
 
-    public void manageDegreeCourse(DegreeCourse dc, String action) {
+    public void manageDegreeCourse(DegreeCourse dc, String action) throws SQLException {
         String sql = "";
         boolean create = false;
         if ("D".equalsIgnoreCase(action)) {
             sql = "Delete from degreecourse where degree = ? and course = ?";
         } else {
             create = true;
-            sql = "Insert into degreecourse (degree, course, semester) values "
-                    + "(?, ?, ?)";
+            sql = "Insert into degreecourse (degree, course, semester, created_date, created_by) values "
+                    + "(?, ?, ?, current_date, ?)";
         }
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, dc.getDegree());
-            ps.setString(2, dc.getCourse());
-            if (create) {
-                ps.setInt(3, dc.getSemester());
-            }
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, dc.getDegree());
+        ps.setString(2, dc.getCourse());
+        if (create) {
+            ps.setInt(3, dc.getSemester());
+            ps.setString(4, SessionUtil.getUserCode());
+        }
 
-            ps.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        ps.executeUpdate();
 
     }
 
@@ -199,7 +217,7 @@ public class SessionBean {
                     + " and (u.role = ? or ? is null)"
                     + " and (lower(u.code) = lower(?) or ? is null)";
             if (name != null) {
-                sql = sql + " and (lower(u.name) like lower('%" + name + "%')";
+                sql = sql + " and lower(u.name) like lower('%" + name + "%')";
             }
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setBoolean(1, all);
@@ -207,10 +225,6 @@ public class SessionBean {
             ps.setString(3, role);
             ps.setString(4, code);
             ps.setString(5, code);
-            if (name != null) {
-                ps.setString(5, name);
-                ps.setString(6, name);
-            }
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 User b = new User(rs.getString(1), rs.getString(2), rs.getString(3),
@@ -282,6 +296,31 @@ public class SessionBean {
     /**
      * *******************************COURSES***********************************
      */
+    public ArrayList<Course> getCourse(String code, String name) {
+        ArrayList<Course> list = new ArrayList();
+        try {
+            String sql = "Select c.code, c.name, c.credits, c.teacher, u.name"
+                    + "     From course c LEFT JOIN user u"
+                    + "       ON c.teacher = u.code"
+                    + "    Where c.code = ? or ? is null";
+            if (name != null) {
+                sql = sql + " and lower(c.name) like lower('%" + name + "%')";
+            }
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, code);
+            ps.setString(2, code);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Course c = new Course(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5));
+                list.add(c);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
     public ArrayList<StudentCourse> getRegisterCourses(String degree) {
         ArrayList<StudentCourse> courses = new ArrayList<StudentCourse>();
         try {
@@ -356,7 +395,7 @@ public class SessionBean {
 
     public ArrayList<StudentCourse> getStudentCourse(String course) {
         ArrayList<StudentCourse> list = new ArrayList<StudentCourse>();
-        String sql = "Select student, concat(name, ' ', family), grade1, grade2, finalgrade from coursestudent C, user U"
+        String sql = "Select student, concat(name, ' ', family), grade1, grade2, finalgrade from coursestudent c, user u"
                 + "    where course = ?"
                 + "      and student = u.code"
                 + "      and cur_semester = ?"
@@ -426,13 +465,33 @@ public class SessionBean {
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                courses.put(rs.getString(1), rs.getString(2));
+                courses.put(rs.getString(2), rs.getString(1));
             }
         } catch (SQLException ex) {
             Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         return courses;
+    }
+
+    public void manageCourse(Course c, String action) throws SQLException {
+        String sql = "";
+        if ("U".equals(action)) {
+            sql = "UPDATE course SET name=?,credits=?,teacher=?,updated_date=now(),updated_by=? WHERE code = ?";
+        } else {
+            sql = "Insert into course(name, credits, teacher, created_date, created_by, activated, code) "
+                    + " values(?,?,?,now(),?,1,?)";
+        }
+
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setString(1, c.getName());
+        stmt.setInt(2, c.getCredits());
+        stmt.setString(3, c.getTeacher());
+        stmt.setString(4, SessionUtil.getUserCode());
+        stmt.setString(5, c.getCode());
+
+        stmt.executeUpdate();
+
     }
 
     /**
@@ -498,8 +557,91 @@ public class SessionBean {
     }
 
     /**
-     * *******************************OTHER************************************
+     *******************************USER
+     * LOGS************************************
      */
+    public int getLastUserId(String user) {
+        int res = 0;
+        try {
+            String sql = "SELECT MAX(id) FROM user_logs WHERE user = ? ";
+            PreparedStatement stmnt = connection.prepareStatement(sql);
+            stmnt.setString(1, user);
+            ResultSet result = stmnt.executeQuery();
+            if (result.next()) {
+                res = result.getInt(1);
+            } else {
+                res = 0;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+
+    }
+
+    public void insertUserLog(String user, String action) {
+        try {
+            String sql = "";
+            int id = getLastUserId(user);
+            if ("U".equals(action)) {
+                sql = "UPDATE user_logs SET logout_date = current_date WHERE user=? AND id=?";
+            } else if ("I".equals(action)) {
+                id++;
+                sql = "INSERT INTO user_logs (user, login_date, id) VALUES (?,current_date,?)";
+            }
+            PreparedStatement stmnt = connection.prepareStatement(sql);
+            stmnt.setString(1, user);
+            stmnt.setInt(2, id);
+            stmnt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ArrayList<UserLogs> getUserLogs() {
+        ArrayList<UserLogs> list = new ArrayList<UserLogs>();
+        try {
+            String sql = "SELECT user, login_date, logout_date, id FROM user_logs order by ifnull(logout_date, login_date)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new UserLogs(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4)));
+            }
+            return list;
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    /**
+     *******************************OTHER************************************
+     *
+     */
+    public Global getGlobal() throws SQLException {
+        String sql = "SELECT curr_semester, curr_year, period, currency, opening_days FROM global";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        Global global = null;
+        while (rs.next()) {
+            global = new Global(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+        }
+        return global;
+    }
+
+    public void manageGlobal(String curr_semester, String year, String period, String currency, String opening_days) throws SQLException {
+        String sql = "UPDATE global SET curr_semester=?, curr_year=?, period=?, currency=?, opening_days=?, updated_by =?, updated_date = current_date";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, curr_semester);
+        ps.setString(2, year);
+        ps.setString(3, period);
+        ps.setString(4, currency);
+        ps.setString(5, opening_days);
+        ps.setString(6, SessionUtil.getUserCode());
+
+        ps.executeUpdate();
+    }
+
     public void delete(String table, String code) {
         try {
             PreparedStatement stmt = connection.prepareStatement("Delete from " + table + " where code = ?");
@@ -522,4 +664,67 @@ public class SessionBean {
         return false;
     }
 
+    /**
+     * *******************************HISTORY************************************
+     */
+    public Map<String, String> getTables() {
+
+        Map<String, String> tables = new LinkedHashMap();
+        try {
+            String sql = "SELECT table_name FROM information_schema.tables where UPPER(table_schema) ='UNI' and UPPER(table_name) not like '%_HIST' "
+                    + "   and table_name in (select substr(table_name,1, length(table_name) -5)"
+                    + "                        from information_schema.tables "
+                    + "                       where UPPER(table_schema) ='UNI' "
+                    + "                         and UPPER(table_name) like '%_HIST') order by table_name";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                tables.put(rs.getString(1), rs.getString(1));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return tables;
+    }
+
+    public ArrayList<String> getColumns(String table) {
+        table = table + "_hist";
+        ArrayList<String> columns = new ArrayList<String>();
+        try {
+            String sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE UPPER(TABLE_SCHEMA) = 'UNI'"
+                    + "  AND UPPER(TABLE_NAME) = UPPER(?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, table);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                columns.add(rs.getString(1));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return columns;
+    }
+
+    public ArrayList<ArrayList<String>> getHistory(String table) {
+        ArrayList<ArrayList<String>> history = new ArrayList<ArrayList<String>>();
+        try {
+            String sql = "SELECT * FROM " + table + "_hist  order by ifnull(updated_date,created_date) desc, created_date desc";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int colsNumber = rsmd.getColumnCount();
+            //
+            while (rs.next()) {
+                ArrayList<String> columns = new ArrayList<String>();
+                for (int i = 1; i <= colsNumber; i++) {
+                    columns.add(rs.getString(i));
+                }
+                history.add(columns);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return history;
+    }
 }
